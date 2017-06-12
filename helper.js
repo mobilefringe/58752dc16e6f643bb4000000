bf637791e669a2f33b89973ff40d2d2f520e7262
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-12 11:51:00 -0400

    Northland Mall 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -71,4 +71,4 @@ function show_content(){
     renderFeatureItems('#feature_item','#feature_item_template', one_item);
     var two_items = feature_items.slice(1,3);
     renderFeatureItems('#home_feature','#home_feature_template', two_items);
-    
+}
