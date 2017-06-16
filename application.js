3328fcf3b8c8bf2097dab9126ee050885a6aa54b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-16 09:19:45 -0400

    Northland Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -437,7 +437,7 @@ function renderStoreDetails(container, template, collection, slug){
     item_list.push(collection);
     $.each( item_list , function( key, val ) {
         if ((val.store_front_url).indexOf('missing.png') > -1){
-            val.alt_store_front_url = "//www.mallmaverick.com/photos/original/missing.png";
+            val.alt_store_front_url = "//codecloud.cdn.speedyrails.net/sites/58752dc16e6f643bb4000000/image/png/1497619091000/logo.png";
         } else {
             val.alt_store_front_url = getImageURL(val.store_front_url); 
         }
